252c7fde8ee2df0ab34cd2e04870ee53efc8bf21
Kevin T <kevintse1996@gmail.com>
2015-07-24 23:57:55 -0400

    added comment

--- P7.py
+++ P7.py
@@ -1,25 +1,25 @@
 # looking for the nth prime
 
 def P7(n): 
-	i = 0
-	pli = [2] # a list to store the prime number
-	x = pli[i]
+	pli = [2] # a list to store the prime numbers
+	i = 0 # list index
+	x = pli[i] # the incrementing number which the loop will check if it is Prime
 	while((i + 1) < n):
 		x += 1
-		if (isPrime(x,pli,i)): # 
-			i += 1
-			pli.append(x)
+		if (isPrime(x,pli,i)): #
+			i += 1 # increment the index
+			pli.append(x) # add the new prime number to list
 			x += 1
 	return pli[i]
 
 def isPrime(n,pli,i):
 	j = 0
-	while j <= i:
+	while j <= i: # walk through the pli to see if n can be factored by any element
 		if (n % pli[j]) == 0:
 			return False
 		else:
 			j += 1
-	return True
+	return True # if n cannot be factored by any prime number, it is prime
 
 # Consider using the fact that prime number are in the form
 # 6n + 1 or 6n - 1 after 2, 3 to improve performance.
